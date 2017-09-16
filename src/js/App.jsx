c7f58751027192aea4f72d1945bea54349498c3e
Amir Mamaghani <mamaghani@biosdesign.eu>
2017-09-16 17:49:50 +0200

    Homepage skelet almost ready

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -5,7 +5,6 @@ import store from './store';
 
 import styles from '../scss/app.scss';
 
-import Primary from './partials/navigation/primary';
 import Home from './views/Home';
 import Search from './views/Search';
 import About from './views/About';
@@ -18,11 +17,10 @@ export default class App extends Component {
       <Provider store={store}>
         <Router>
           <div className={styles.root}>
-            <Primary />
             <Switch>
               <Route exact path="/" component={Home} />
-			  <Route path="/hladanie" component={Search} />
-			  <Route path="/about" component={About} />
+              <Route path="/hladanie" component={Search} />
+              <Route path="/about" component={About} />
               <Route component={NotFound} />
             </Switch>
           </div>
--- src/js/views/Home/index.jsx
+++ src/js/views/Home/index.jsx
@@ -1,18 +1,79 @@
-import React, { Component } from 'react';
-import { Button, Container, Col, Row } from 'reactstrap';
-import styles from '../../../scss/views/home.scss';
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Container, Row, Col } from 'reactstrap';
+import IconBath from 'react-icons/lib/fa/beer';
+import styles from './home.scss';
+import { Link } from 'react-router-dom';
 
-export default class extends Component {
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <div className={styles.container}>
-        <Row>
-          <Col xs={12}>
-            Serus'
-          </Col>
-        </Row>
+
+        <Navbar color="white" light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/">&nbsp;</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Prihlasenie</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">O projekte</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+
+        <h1 className={styles.mainTitle}>Vyhladajte spomedzi 1000 inzeratov</h1>
+
+        <Container>
+
+          <Row className={styles.propertyTypeSelector}>
+
+            <Col md="4" xs="6"><Link to={'/'}><IconBath /> <span>Byty</span></Link></Col>
+            <Col md="4" xs="6"><Link to={'/'}><IconBath /> <span>Domy</span></Link></Col>
+            <Col md="4" xs="6"><Link to={'/'}><IconBath /> <span>Projekty</span></Link></Col>
+
+          </Row>
+
+          <Row className={styles.propertyTypeSelector}>
+            <Col md="4" xs="6"><Link to={'/'}><IconBath /> <span>Pozemky</span></Link></Col>
+            <Col md="4" xs="6"><Link to={'/'}><IconBath /> <span>Komercne</span></Link></Col>
+            <Col md="4" xs="6"><Link to={'/'}><IconBath /> <span>Ostatne</span></Link></Col>
+          </Row>
+
+        </Container>
+
+
+        <Container className={styles.searchOptions}>
+          <Row>
+            <Col xs="12">
+              <h4>Byty na predaj</h4>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs="12">
+              <h4>Oblubene</h4>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
 }
-
