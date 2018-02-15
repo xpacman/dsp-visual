9fbb5902febc31630f99aa92967dfe5bc7082a6e
Ondro Pacas <paco16@centrum.sk>
2018-02-15 19:27:01 +0100

    Approximations done, Crosshairs still buggy in some cases

---
+++ src/js/views/Regression/config.js
@@ -0,0 +1,40 @@
+/**
+ * Created by paco on 14.2.18.
+ */
+
+module.exports = {
+  crosshairLine: {
+    stroke: "#292b2c"
+  },
+  crosshairText: {
+    fill: "#494949",
+    fontSize: 14,
+    offsetX: 10,
+    align: "center"
+  },
+  axisTickLine: {
+    stroke: "#292b2c",
+    strokeWidth: 1
+  },
+  axisTick: {
+    fill: "#494949",
+    fontSize: 14,
+    offsetX: 10,
+    align: "center"
+  },
+  axisLine: {
+    stroke: "#494949",
+    strokeWidth: 1
+  },
+  pointCross: {
+    stroke: "white"
+  },
+  pointCircle: {
+    radius: 3,
+    fill: "red"
+  },
+  approximationLine: {
+    stroke: "green",
+    strokeWidth: 3
+  }
+};
--- src/js/views/Regression/index.jsx
+++ src/js/views/Regression/index.jsx
@@ -1,33 +1,47 @@
 import React from 'react';
-import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem, InputGroup, Input} from 'reactstrap';
+import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Label, InputGroup, Input, FormGroup} from 'reactstrap';
 import styles from './regression.scss';
 import CanvasManager from '../../utils/CanvasManager';
-import {Stage, Layer, Rect, Line, Text, Group} from 'react-konva';
+import {Stage, Layer, Rect, Line, Text, Group, Circle} from 'react-konva';
 import {max, min} from 'd3-array';
+import linear from 'linear-solve';
+import config from './config';
 import Konva from 'konva';
 
 
 export default class Regression extends React.Component {
   constructor(props) {
     super(props);
+    // Default input values
+    const inputValues = [5, 70, 140, 23, 250, 60, 300, 20];
     this.state = {
       dropdowns: {
         inputValues: false,
-        aproximation: false
+        approximation: false
       },
-      inputValues: [5, 70, 140, 23, 250, 60, 300, 20],
+      inputValues: inputValues,
+      selectedApproximation: 'line'
     };
 
     this.canvasManager = new CanvasManager();
-    this.chartMargins = [0, 75, 100, 0]; // Margins top, right, bottom, right and bottom are greater because we need leave space for axes
+    this.approximationResults = {}; // Approximations will be placed here
+    this.chartMargins = [0, 75, 100, 0]; // Margins top, right, bottom, left, right and bottom are greater because we need leave space for axes
     this.xTickMargins = [20, 0];
     this.yTickMargins = [0, 20];
     this.inputValues = null; // Input values ref
     this.chartWrapper = null; // Chart wrapper ref
+    this.stage = null; // Konva stage ref
+    this.crosshairsLayer = null; // Crosshairs layer ref
+    this.pointsLayer = null; // Points layer ref
+    this.axisLayer = null; // Axes layer ref
   }
 
   componentDidMount() {
-    setTimeout(() => this.forceUpdate(), 1000); // Delayed mount of konva chart
+    setTimeout(() => {
+      // Delayed mount of konva chart
+      this.forceUpdate();
+    }, 1000);
+
   }
 
   componentWillUpdate(nextProps, nextState) {
@@ -47,6 +61,7 @@ export default class Regression extends React.Component {
         // From bottom edge to top edge
         this.canvasManager.yRange = [this.canvasManager.dimensions[1] - this.chartMargins[2], 0];
         this.canvasManager.rescale();
+        this.approximationResults = this.getApproximations(nextState.inputValues);
         console.log("rescaled");
       }
     }
@@ -87,8 +102,73 @@ export default class Regression extends React.Component {
     return points;
   }
 
+  /**
+   * Calculates linear, parabolic and exponential approximations and returns object containing arrays of points
+   * @param inputValues
+   */
+  getApproximations(inputValues) {
+    // Number of points [x y]
+    const n = inputValues.length / 2;
+
+    // Variables inicialization
+    let xi = 0;
+    let yi = 0;
+    let xiPow = 0;
+    let xiPow_3 = 0;
+    let xiPow_4 = 0;
+    let xiyi = 0;
+    let xiPowYi = 0;
+    let xi_lnyi = 0;
+    let lnyi = 0;
+
+    inputValues.map((value, index) => {
+      // X value
+      if (index % 2 === 0) {
+        xi += value;
+        xiPow += Math.pow(value, 2);
+        xiPow_3 += Math.pow(value, 3);
+        xiPow_4 += Math.pow(value, 4);
+      } else {
+        // Y value
+        yi += value;
+        xiyi += value * inputValues[index - 1];
+        xiPowYi += value * Math.pow(inputValues[index - 1], 2);
+        xi_lnyi += inputValues[index - 1] * Math.log2(value);
+        lnyi += Math.log2(value);
+      }
+    });
+
+    // Get coefficients
+    const lineA = (n * xiyi - xi * yi) / (n * xiPow - Math.pow(xi, 2));
+    const lineB = (xiPow * yi - xi * xiyi) / (n * xiPow - Math.pow(xi, 2));
+    const parabolaCoef = linear.solve([[n, xi, xiPow], [xi, xiPow, xiPow_3], [xiPow, xiPow_3, xiPow_4]], [yi, xiyi, xiPowYi]);
+    const exponentialB = (n * xi_lnyi - xi * lnyi) / (n * xiPow - Math.pow(xi, 2));
+    const exponentialA = (1 / n) * Math.log2(yi) - (exponentialB / n) * xi;
+
+    // Calculate final approximations
+    const approx = {
+      line: [],
+      parabola: [],
+      exponential: []
+    };
+    inputValues.map((value, index) => {
+      if (index % 2 === 0) {
+        approx.line.push(this.canvasManager.xScale(value));
+        approx.parabola.push(this.canvasManager.xScale(value));
+        approx.exponential.push(this.canvasManager.xScale(value));
+        // y = ax + b
+        approx.line.push(this.canvasManager.yScale(lineA * value + lineB));
+        // y = c0 + c1x + c2x^2
+        approx.parabola.push(this.canvasManager.yScale(parabolaCoef[0] + parabolaCoef[1] * value + parabolaCoef[2] * Math.pow(value, 2)));
+        // y = e^a+bx
+        approx.exponential.push(this.canvasManager.yScale(Math.exp(exponentialA + exponentialB * value)));
+      }
+    });
+    return approx;
+  }
+
   render() {
-    const {dropdowns, inputValues} = this.state;
+    const {dropdowns, inputValues, selectedApproximation} = this.state;
     // Recalculated points from input values
     let points = [];
     let xTicks = [];
@@ -101,12 +181,112 @@ export default class Regression extends React.Component {
       yTicks.shift();
     }
 
+    let approxLineProps = {
+      ...config.approximationLine
+    };
+    let approxLabel = "";
+    switch (selectedApproximation) {
+      case 'line':
+        approxLabel = "Přímka";
+        approxLineProps = {
+          ...approxLineProps,
+          points: this.approximationResults.line,
+          tension: 0.1,
+          lineCap: 'round',
+          lineJoin: 'round',
+        };
+        break;
+      case 'parabola':
+        approxLabel = "Parabola";
+        approxLineProps = {
+          ...approxLineProps,
+          points: this.approximationResults.parabola,
+          tension: 0.3,
+          lineCap: 'round',
+          lineJoin: 'round',
+        };
+        break;
+      case 'exponential':
+        approxLabel = "Exponenciála";
+        approxLineProps = {
+          ...approxLineProps,
+          points: this.approximationResults.exponential,
+          tension: 0.3,
+          lineCap: 'round',
+          lineJoin: 'round',
+        };
+        break;
+      default:
+        break;
+    }
+
     return (
-      <div className={styles.container}>
+      <div className={styles.container}
+           onMouseMove={() => {
+             // Crosshairs handling
+             if (this.crosshairsLayer !== null && this.stage !== null) {
+               const cursorPosition = this.stage.getStage().getPointerPosition();
+               if (cursorPosition) {
+                 const dimensionsWithMargins = [this.canvasManager.dimensions[0] - this.chartMargins[1] - this.chartMargins[3], this.canvasManager.dimensions[1] - this.chartMargins[2]];
+                 if (cursorPosition.x > dimensionsWithMargins[0]) {
+                   cursorPosition.x = dimensionsWithMargins[0]
+                 }
+                 if (cursorPosition.y > dimensionsWithMargins[1]) {
+                   cursorPosition.y = dimensionsWithMargins[1]
+                 }
+                 // Crosshair line and text are grouped for synchronized moving
+                 const xCrosshairGroup = this.crosshairsLayer.getChildren()[0];
+                 const yCrosshairGroup = this.crosshairsLayer.getChildren()[1];
+                 xCrosshairGroup.setAttr("x", cursorPosition.x);
+                 yCrosshairGroup.setAttr("y", cursorPosition.y);
+                 const xText = xCrosshairGroup.getChildren()[1];
+                 xText.setAttr("text", (this.canvasManager.xDomain[1] * (100 / (this.canvasManager.dimensions[0] - this.chartMargins[1]) * xCrosshairGroup.getPosition().x) / 100).toFixed(3));
+                 const yText = yCrosshairGroup.getChildren()[1];
+                 //xText.setAttr("text", this.canvasManager.xScale(xCrosshairGroup.getPosition().x));
+                 this.crosshairsLayer.getLayer().batchDraw();
+               }
+             }
+           }}>
         <div className={styles.topOptionsBar}>
-          <div className={`${styles.topOptionsBar__item}`}>
-            Aproximace
-          </div>
+          <Dropdown className={`${styles["topOptionsBar__item--dropdown"]}`}
+                    isOpen={dropdowns.approximation}
+                    toggle={this.toggleDropdown.bind(this, "approximation")}>
+            <DropdownToggle
+              tag="span"
+              className={`${styles["topOptionsBar__item--dropdown__dropdownToggle"]}`}
+              onClick={this.toggleDropdown.bind(this, "approximation")}
+              data-toggle="dropdown"
+              aria-expanded={dropdowns.approximation}
+              caret>
+              Aproximace: {approxLabel}
+            </DropdownToggle>
+            <DropdownMenu className={`pl-2 ${styles["topOptionsBar__item--dropdown__dropdownMenu"]}`}>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" checked={selectedApproximation === "line"}
+                         onChange={(event) => this.setState({selectedApproximation: event.target.name})}
+                         name="line"/>{' '}
+                  Přímka
+                </Label>
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" checked={selectedApproximation === "parabola"}
+                         onChange={(event) => this.setState({selectedApproximation: event.target.name})}
+                         name="parabola"/>{' '}
+                  Parabola
+                </Label>
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" checked={selectedApproximation === "exponential"}
+                         onChange={(event) => this.setState({selectedApproximation: event.target.name})}
+                         name="exponential"/>{' '}
+                  Exponenciála
+                </Label>
+              </FormGroup>
+            </DropdownMenu>
+          </Dropdown>
           <Dropdown className={`${styles["topOptionsBar__item--dropdown"]}`}
                     isOpen={dropdowns.inputValues}
                     toggle={this.toggleDropdown.bind(this, "inputValues")}>
@@ -119,7 +299,8 @@ export default class Regression extends React.Component {
               caret>
               Vstupní hodnoty
             </DropdownToggle>
-            <DropdownMenu className={`${styles["topOptionsBar__item--dropdown__dropdownMenu"]}`}>
+            <DropdownMenu
+              className={`${styles.inputValuesDropdown} ${styles["topOptionsBar__item--dropdown__dropdownMenu"]}`}>
               <InputGroup>
                 <Input placeholder="x1, y1, x2, y2,..."
                        type="text"
@@ -141,17 +322,13 @@ export default class Regression extends React.Component {
         <div id="regression-chart-wrapper" ref={(elem) => {
           this.chartWrapper = elem
         }} className="h-100 w-100">
-          <Stage width={this.canvasManager.dimensions[0]}
+          <Stage ref={(stage) => this.stage = stage}
+                 width={this.canvasManager.dimensions[0]}
                  height={this.canvasManager.dimensions[1]}>
-            <Layer>
-              <Line points={points}
-                    stroke="green"
-              />
-            </Layer>
-            <Layer>
+            <Layer ref={(layer) => this.axisLayer = layer}>
               <Line
                 points={[0, this.canvasManager.dimensions[1] - this.chartMargins[2], this.canvasManager.dimensions[0], this.canvasManager.dimensions[1] - this.chartMargins[2]]}
-                stroke="#494949"
+                {...config.axisLine}
               />
               {
                 xTicks.map((tick, index) => {
@@ -162,21 +339,18 @@ export default class Regression extends React.Component {
                       text={tick}
                       x={0}
                       y={0}
-                      fill="#494949"
-                      fontSize={14}
-                      offsetX={10}
-                      align="center"
+                      {...config.axisTick}
                     />
                     <Line
                       points={[0, -this.xTickMargins[0], 0, -this.canvasManager.dimensions[1]]}
-                      stroke="#292b2c"
+                      {...config.axisTickLine}
                     />
                   </Group>
                 })
               }
               <Line
                 points={[this.canvasManager.dimensions[0] - this.chartMargins[1], 0, this.canvasManager.dimensions[0] - this.chartMargins[1], this.canvasManager.dimensions[1]]}
-                stroke="#494949"
+                {...config.axisLine}
               />
               {
                 yTicks.map((tick, index) => {
@@ -187,17 +361,55 @@ export default class Regression extends React.Component {
                       text={tick}
                       x={0}
                       y={0}
-                      fill="#494949"
-                      fontSize={14}
-                      align="center"
+                      {...config.axisTick}
                     />
                     <Line
                       points={[-this.yTickMargins[1], 0, -this.canvasManager.dimensions[0] - this.chartMargins[1], 0]}
-                      stroke="#292b2c"
+                      {...config.axisTickLine}
                     />
                   </Group>
                 })
               }
+            </Layer>
+            <Layer ref={(layer) => this.crosshairsLayer = layer}>
+              <Group x={0}
+                     y={0}>
+                <Line points={[0, 0, 0, this.canvasManager.dimensions[1] - this.chartMargins[2]]}
+                      {...config.crosshairLine}
+                      name="xCrosshair"
+                />
+                <Text text={""}
+                      x={0}
+                      y={this.canvasManager.dimensions[1] - this.chartMargins[2] + this.xTickMargins[0]}
+                      {...config.crosshairText}
+                />
+              </Group>
+              <Group x={0}
+                     y={0}>
+                <Line points={[0, 0, this.canvasManager.dimensions[0] - this.chartMargins[1], 0]}
+                      {...config.crosshairLine}
+                      name="yCrosshair"
+                />
+                <Text text={""}
+                      x={0}
+                      y={0}
+                      {...config.crosshairText}
+                />
+              </Group>
+            </Layer>
+            <Layer ref={(layer) => this.pointsLayer = layer}>
+              {
+                points.map((point, index) => {
+                  if (index % 2 === 0) {
+                    return <Group key={index} x={point} y={points[index + 1]}>
+                      <Line points={[-10, 0, 10, 0]} {...config.pointCross}/>
+                      <Line points={[0, -10, 0, 10]}{...config.pointCross}/>
+                      <Circle {...config.pointCircle} x={0} y={0}/>
+                    </Group>
+                  }
+                })
+              }
+              <Line {...approxLineProps}/>
             </Layer>
           </Stage>
         </div>
